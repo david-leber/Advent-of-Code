9f71ad2c1e23bde740a70c364c98b7eb3b383149
David Leber <david.a.leber@gmail.com>
2022-12-01 09:44:31 -0800

    Add token, fix bugs

--- 2022/2022_01.py
+++ 2022/2022_01.py
@@ -1,12 +1,20 @@
-"""Year 2022 Day 1
+"""Year 2022 Day 1.
 
 By: David Leber
 """
 
-from aocd.models import Puzzle
+from aocd.models import Puzzle, User
 from queue import PriorityQueue
-puzzle = Puzzle(year=2022, day=1)
-data = puzzle.input_data.split('\n')
+
+year = 2022
+day = 1
+
+with open(f"{year}/session.txt", "r") as fh:
+    token = fh.read()
+
+user = User(token=token)
+puzzle = Puzzle(year=year, day=day, user=user)
+data = puzzle.input_data.split("\n")
 
 
 top_found = PriorityQueue()
@@ -16,7 +24,9 @@ for i in data:
     if i != "":
         running_total += int(i)
     else:
-        top_found.put(running_total)
-    
-puzzle.answer_a = sum(top_found.queue[-1:])
-puzzle.answer_a = sum(top_found.queue[-3:])
+        top_found.put(-running_total)
+        running_total = 0
+
+puzzle.answer_a = sum([-top_found.get() for _ in range(1)])
+top_found.put(-int(puzzle.answer_a))
+puzzle.answer_b = sum([-top_found.get() for _ in range(3)])
